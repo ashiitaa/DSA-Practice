c1ecd5387fd60c056c2aeb3ce37119e6e84f58aa
ashiitaa <ashita2911patel@gmail.com>
2026-05-02 08:59:09 +0530

    practice

---
+++ Arrays/practice.py
@@ -0,0 +1,24 @@
+# COUNT EVEN NUMBERS
+def count_even(arr):
+  count = 0
+  for num in arr:
+   if  num % 2 == 0:
+     count = count + 1 
+
+  return count  
+
+arr = [3,4,8,6,9]
+
+print ('even num are:', count_even(arr))
+
+# FIND SUM OF ARRAY
+def find_sum(arr):
+  total = 0
+
+for num in arr :
+  total = total + num 
+return total 
+
+arr = [3,5,6,7,1]
+
+print = ('the sum is:', find_sum(arr))
