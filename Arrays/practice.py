dfe52722eb1d1ff21b3192b57958e7b3ce8c4bcd
ashiitaa <ashita2911patel@gmail.com>
2026-05-02 09:13:07 +0530

    practice

--- Arrays/practice.py
+++ Arrays/practice.py
@@ -22,3 +22,15 @@ return total
 arr = [3,5,6,7,1]
 
 print = ('the sum is:', find_sum(arr))
+# COUNT POSITIVE NUMBERS
+def count_positive(arr):
+    count = 0 
+    for num in arr:
+        if num > 0 :
+           count = count + 1
+      
+    return count  
+
+arr = [2,-6,-3,4,-9]
+
+print('total positive numbers are:', count_positive(arr))
