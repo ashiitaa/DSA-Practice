ad901ddf1e55ecd254b7fc1a72d01ec2afc46256
ashiitaa <ashita2911patel@gmail.com>
2026-05-12 08:01:15 +0530

    Update max_element.py

--- Arrays/max_element.py
+++ Arrays/max_element.py
@@ -11,3 +11,23 @@ print('maximum element is:',
 find_max(arr))
 #time complexity : 0(n)
 # space complexity : 0(1)
+# MOVE ALL ZERO TO THE END OF ARRAY
+def move_zero(arr):
+    new_arr = []
+    zero = 0 
+
+    for num in arr:
+
+        if num != 0:
+          new_arr.append(num)
+        else :
+           zero = zero + 1 
+
+    for i in range(zero):
+       
+       new_arr.append(0)
+
+    return new_arr
+
+arr = [3,0,0,8,5,0,2]
+print('new array is:', move_zero(arr))
